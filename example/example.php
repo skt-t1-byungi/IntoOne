906430f4f760e1b8d8a34200cd873cb2ebfeeafc
skt-t1-byungi <sertion@innorix.com>
2016-11-20 23:52:09 +0900

    fix exmaples

--- example/example.php
+++ example/example.php
@@ -1,13 +1,16 @@
 <?php
+$path = "test.bin";
 
 IntoOne::concat($path, function ($add) {
-    $add->data($key, $data);
-    $add->path($key, $path);
-    $add->resource($key, $resource);
+    $add->data('1', 'abcd');
+    $add->path('2', 'files/test.txt');
+    $add->resource('3', fopen('php://stdin', 'r'));
 });
 
-$data = IntoOne::read($path, $name);
+$data = IntoOne::read($path, '1'); // $data == 'abcd'
 
-IntoOne::readChunks($path, $name, function ($chunk) {
-
+$content = '';
+IntoOne::readChunks($path, 2, function ($chunk) use ($content) {
+    $content .= $chunk;
 });
+//$content == file_get_contents(files/test.txt)
